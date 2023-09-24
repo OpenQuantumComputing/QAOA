1fdc9255ed6726674ad8159357f72248e4fd17f0
Franz Georg Fuchs <franzgeorgfuchs@gmail.com>
2023-09-24 21:49:48 +0200

    Version1 (#6)
    version 1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="qaoa",
-    version="0.2",
+    version="1.0",
     license="GNU General Public License v3.0",
     author="Franz Georg Fuchs",
     author_email="franzgeorgfuchs@gmail.com",
     description="Quantum Alternating Operator Ansatz/Quantum Approximate Optimization Algorithm (QAOA)",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["examples", "images"]),
     keywords="quantum computing, qaoa, qiskit",
     install_requires=[
         "numpy",
