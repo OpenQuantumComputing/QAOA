046fd99d07f9f9c426535f36cb22373d55fba116
Franz Fuchs <franzgeorgfuchs@gmail.com>
2024-08-07 09:34:14 +0200

    refactoring

--- qaoa/mixers/x_mixer.py
+++ qaoa/mixers/x_mixer.py
@@ -5,9 +5,11 @@ from .base_mixer import Mixer
 
 
 class X(Mixer):
+    def __init__(self) -> None:
+        self.mixer_param = Parameter("x_beta")
+
     def create_circuit(self):
         q = QuantumRegister(self.N_qubits)
-        mixer_param = Parameter("x_beta")
 
         self.circuit = QuantumCircuit(q)
-        self.circuit.rx(-2 * mixer_param, range(self.N_qubits))
+        self.circuit.rx(-2 * self.mixer_param, range(self.N_qubits))
