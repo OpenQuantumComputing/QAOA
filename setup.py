871f3d4140d91f1e331d36cd268a9b8fefa8ba3b
Franz Fuchs <franzgeorgfuchs@gmail.com>
2024-08-22 09:18:39 +0200

    updated version. must use qiskit 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qaoa",
-    version="1.0.4",
+    version="1.1.0",
     license="GNU General Public License v3.0",
     author="Franz Georg Fuchs",
     author_email="franzgeorgfuchs@gmail.com",
@@ -18,7 +18,7 @@ setup(
         "matplotlib",
         "networkx",
         "jupyter",
-        "qiskit>=1.0",
+        "qiskit==1.1.1",
         "qiskit-aer",
         "qiskit-algorithms",
         "qiskit-finance",
