30e93c0c7bda3b3d90c19c979aeb33cfab8d3011
sannesg <Sanne.selmer.gilje@sintef.no>
2025-06-13 12:40:55 +0200

    documentaition for statevector_initialstate

--- qaoa/initialstates/statevector_initialstate.py
+++ qaoa/initialstates/statevector_initialstate.py
@@ -5,11 +5,20 @@ from .base_initialstate import InitialState
 
 
 class StateVector(InitialState):
+    """
+    Creates the initial statevector
+
+    Methods:
+        create_circuit(): Creates a circuit that creates the initial statevector
+    """
     def __init__(self, statevector) -> None:
         super().__init__()
         self.statevector = statevector
 
     def create_circuit(self):
+        """
+        Creates a circuit that makes the initial statevector
+        """
         q = QuantumRegister(self.N_qubits)
         self.circuit = QuantumCircuit(q)
         self.circuit.initialize(self.statevector, q)
