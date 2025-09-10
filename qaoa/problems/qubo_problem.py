af9e57ce76b7addef1b5efdb7bef054ac3d36891
havahol <havard.heitlo.holm@sintef.no>
2025-09-10 15:03:13 +0200

    Fixed circuit for generic QUBO problem, incl validation

--- qaoa/problems/qubo_problem.py
+++ qaoa/problems/qubo_problem.py
@@ -6,6 +6,9 @@ from qiskit.circuit import Parameter
 
 from .base_problem import Problem
 
+import structlog
+LOG = structlog.get_logger(file=__name__)
+
 
 class QUBO(Problem):
     """
@@ -19,7 +22,7 @@ class QUBO(Problem):
         c (np.ndarray): A 1-dimensional numpy ndarray representing the linear coefficients.
         b (float): Scalar offset term.
         N_qubits (int): Number of binary variables/qubits in the problem.
-        lower_triangular_Q (bool): Whether Q is lower triangular.
+        is_lower_triangular_Q (bool): Whether Q is lower triangular.
         QUBO_Q (np.ndarray): The quadratic coefficient matrix.
         QUBO_c (np.ndarray): The linear coefficient vector.
         QUBO_b (float): The scalar offset.
@@ -61,9 +64,6 @@ class QUBO(Problem):
 
         self.N_qubits = n
 
-        # Check if Q is lower triangular
-        self.lower_triangular_Q = np.allclose(Q, np.tril(Q))
-
         self.QUBO_Q = Q
 
         if c is None:
@@ -102,47 +102,42 @@ class QUBO(Problem):
 
     def create_circuit(self):
         """
-        Creates a parametrized quantum circuit corresponding to the cost function of the QUBO problem.
+        Creates a parameterized quantum circuit that corresponds to the QUBO cost function
+        C(x) = x^T Q x + c^T x 
+        """
 
-        Raises:
-            NotImplementedError: If Q is not lower triangular.
-        """
-        if not self.lower_triangular_Q:
-            LOG.error("Function not implemented!", func=self.create_circuit.__name__)
-            raise NotImplementedError
-        self.createParameterizedCostCircuitTril()
+        # LOG.info("Creating parameterized cirquit for generic QUBO problem")
 
-    def createParameterizedCostCircuitTril(self):
-        """
-        Creates a parameterized circuit of the triangularized QUBO problem.
-        """
-        q = QuantumRegister(self.N_qubits)
+        # To simplify notation:
+        N = self.N_qubits
+        Q = self.QUBO_Q
+        c = self.QUBO_c
+        gamma = Parameter("x_gamma")
+
+        # Ensure that Q is symmetric and add c to its diagonal
+        Q = 0.5*(Q+Q.T) + np.diag(c)
+
+        # Creating parameters b for rz gates, 
+        # where b_i = -sum_j(Q_ij + Q_ji) = -2*sum(Q[:,i]) from symmetry
+        b = -2.0 * Q.sum(axis=0)
+
+        q = QuantumRegister(N)
         self.circuit = QuantumCircuit(q)
-        cost_param = Parameter("x_gamma")
 
-        ### cost Hamiltonian
-        for i in range(self.N_qubits):
-            w_i = 0.5 * (self.QUBO_c[i] + np.sum(self.QUBO_Q[:, i]))
 
-            if not math.isclose(w_i, 0, abs_tol=1e-7):
-                self.circuit.rz(cost_param * w_i, q[i])
+        # Add rz gates:
+        # Note that we use multiply with factor 2 since qiskit implements exp^(-i theta/2 Z)
+        for i in range(N):
+            if not math.isclose(b[i], 0, abs_tol=1e-10):
+                # LOG.info("Adding rz gate to qubit "+str(i)+" with parameter b[i] = "+str(b[i]))
+                self.circuit.rz(2*0.25*b[i]*gamma, i)
 
-            for j in range(i + 1, self.N_qubits):
-                w_ij = 0.25 * self.QUBO_Q[j][i]
+        # Add rzz gates:
+        # Since Q now is symmetric, we apply the RZZ-gate only to its lower triangular part while multiplying by 2 
+        # We also multiply with factor 2 since qiskit implements exp^(-i theta/2 Z tensor Z)
+        for i in range(N):
+            for j in range(i+1, N):
+                if not (math.isclose(Q[i, j], 0.0, abs_tol=1e-10)):
+                    # LOG.info("Adding rzz gate between qubits "+str(i)+" and "+str(j)+" with parameter Q[i,j] = "+str(Q[i, j]))
+                    self.circuit.rzz(2*2*0.25*Q[i,j]*gamma, i, j)        
 
-                if not math.isclose(w_ij, 0, abs_tol=1e-7):
-                    self.circuit.cx(q[i], q[j])
-                    self.circuit.rz(cost_param * w_ij, q[j])
-                    self.circuit.cx(q[i], q[j])
-
-    # def __str2np(self, s):
-    #    x = np.array(list(map(int, s)))
-    #    assert len(x) == self.N_qubits, (
-    #        "bitstring  "
-    #        + s
-    #        + " of wrong size. Expected "
-    #        + str(self.N_qubits)
-    #        + " but got "
-    #        + str(len(x))
-    #    )
-    #    return x
